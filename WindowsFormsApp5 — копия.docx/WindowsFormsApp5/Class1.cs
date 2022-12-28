607d3fcf8f7c87c45bd921abf20564184138d015
MR14Titan14 <119119731+MR14Titan14@users.noreply.github.com>
2022-12-28 14:00:21 +0300

    Add files via upload

---
+++ WindowsFormsApp5 — копия.docx/WindowsFormsApp5/Class1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Lab15
+{
+
+
+
+
+    public class Graph
+    {
+        public class Node
+        {
+            public int id;
+            public int active;
+            public int x;
+            public int y;
+            public string stringId;
+            public int mark = 0;
+            public List<int> edges;
+            public SolidBrush nodeColor = new SolidBrush(Color.Black);
+
+            public void addEdge(int id)
+            {
+                if (!edges.Contains(id)) edges.Add(id);
+            }
+
+            public void removeEdge(int id)
+            {
+                edges.Remove(id);
+            }
+        };
+
+        public List<Node> nodes = new List<Node>();
+        public List<bool> used = new List<bool>();
+        private int maxId = 0;
+        public int x = 0;
+        public int y = 0;
+        public bool inverted = false;
+        public List<List<int>> matrix = new List<List<int>>();
+        public void addNode()
+        {
+            bool find = false;
+            int id = 0;
+            for (int i = 0; i < maxId; i++)
+            {
+                bool exist = false;
+                foreach (Node nd in nodes)
+                {
+                    if (nd.id == i)
+                    {
+                        exist = true;
+                        break;
+                    }
+                }
+                if (!exist)
+                {
+                    id = i;
+                    find = true;
+                    break;
+                }
+            }
+            if (!find)
+            {
+                id = maxId;
+                maxId++;
+            }
+            Node n = new Node();
+            n.id = id;
+            n.active = 0;
+            n.x = x;
+            n.y = y;
+            n.stringId = id.ToString();
+            n.mark = 0;
+            n.edges = new List<int>();
+            nodes.Add(n);
+
+            used.Add(false);
+            nodes.Sort((x, y) => x.id.CompareTo(y.id));
+
+            matrix.Clear();
+            CreateMatrix();
+        }
+
+        public void RemoveNode(int id)
+        {
+            Node n = null;
+            foreach (Node nd in nodes)
+            {
+                nd.edges.Remove(id);
+                if (nd.id == id)
+                {
+                    n = nd;
+                }
+            }
+            nodes.Remove(n);
+
+            matrix.Clear();
+            CreateMatrix();
+        }
+
+        public void LoadNode(int id, int x, int y, string name, List<int> e)
+        {
+            Node n = new Node();
+            if (maxId <= id)
+                maxId = id + 1;
+            n.id = id;
+            n.active = 0;
+            n.x = x;
+            n.y = y;
+            n.stringId = id.ToString();
+            n.edges = e;
+            nodes.Add(n);
+            used.Add(false);
+            n.mark = 0;
+
+        }
+
+        public void CreateMatrix()
+        {
+            matrix.Clear();
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                List<int> vershina = new List<int>();
+                for (int j = 0; j < nodes.Count; j++)
+                {
+                    if (nodes[i].edges.Contains(j)) vershina.Add(1);
+                    else vershina.Add(0);
+                }
+                matrix.Add(vershina);
+            }
+        }
+
+        public void PoiskStokovIstokov(ref List<int> Stoki, ref List<int> Istoki)
+        {
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                bool stok = true, istok = true;
+                for (int j = 0; j < matrix[i].Count; j++)
+                {
+                    if (matrix[i][j] == 1) stok = false;
+                    if (matrix[j][i] == 1) istok = false;
+                }
+                if (stok && istok) continue;
+                else if (stok) Stoki.Add(i);
+                else if (istok) Istoki.Add(i);
+            }
+        }
+
+        public Node findUnusedVertex(List<Node> list)
+        {
+            foreach (Node n in list)
+            {
+                if (!used[list.IndexOf(n)])
+                    return n;
+            }
+            return null;
+        }
+
+
+    }
+
+    public static class additionFunctions
+    {
+        public static double degToRad(double deg)
+        {
+            return deg * Math.PI / 180;
+        }
+
+        public static double radToDeg(double rad)
+        {
+            return rad / Math.PI * 180;
+        }
+
+        public static double lengthdir_x(double len, double dir)
+        {
+            return len * Math.Cos(degToRad(dir));
+        }
+
+        public static double lengthdir_y(double len, double dir)
+        {
+            return len * Math.Sin(degToRad(dir)) * (-1);
+        }
+
+        public static double point_direction(int x1, int y1, int x2, int y2)
+        {
+            return 180 - radToDeg(Math.Atan2(y1 - y2, x1 - x2));
+        }
+
+        public static double point_distance(int x1, int y1, int x2, int y2)
+        {
+            return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        }
+    }
+
+}
---
+++ WindowsFormsApp5 — копия.docx/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Lab15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(31, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(727, 319);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить вершину";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(518, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Поиск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(660, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(660, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Загрузить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(155, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(137, 20);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Удаление ребер";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(155, 56);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(153, 20);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Добавление ребер";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(339, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(309, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Нахождение истоков и стоков:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
